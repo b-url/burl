a1a8b5ca5b0bc00ba58bec180fa15892200d7539
Maikel Veen <veenmaikel@gmail.com>
2024-09-22 13:31:09 +0200

    chore(api): Generate client library pkg

--- api/generate.go
+++ api/generate.go
@@ -1,3 +1,4 @@
 package api
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=codegen.cfg.v1.yaml openapi.v1.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=oapi-server-v1.yaml openapi.v1.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=oapi-client.yaml openapi.v1.yaml
