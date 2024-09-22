b0360fe067d524678bf8165b8432608a7a1c67c4
Dennis Wethmar <denniswethmar@mbpvandennis2-1.home>
2024-09-22 12:53:52 +0200

    chore: use const instead of var

--- cmd/server/serve.go
+++ cmd/server/serve.go
@@ -13,9 +13,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	readTimeout = time.Second * 5
-)
+const readTimeout = time.Second * 5
 
 // NewServeCMD returns a new serve command.
 func NewServeCMD() *cobra.Command {
