802c96e65da1732ef693f93b7a65fd634f49aa46
Maikel Veen <veenmaikel@gmail.com>
2024-09-27 22:05:36 +0200

    refactor(schema): Make all primary keys uuids

--- migrations/20240926071113_init.sql
+++ migrations/20240926071113_init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
     create_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
@@ -7,33 +7,37 @@ CREATE TABLE users (
 );
 
 CREATE TABLE collections (
-    id BIGSERIAL PRIMARY KEY,
-    parent_id BIGINT NULL REFERENCES collections(id),
-    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    id UUID PRIMARY KEY,
     name TEXT NOT NULL,
+    parent_id UUID NULL REFERENCES collections(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     create_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     update_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE bookmarks (
-    id BIGSERIAL PRIMARY KEY,
-    collection_id BIGINT REFERENCES collections(id) ON DELETE CASCADE,
-    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    id UUID PRIMARY KEY,
+    collection_id UUID REFERENCES collections(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     url TEXT NOT NULL,
     title TEXT NOT NULL,
-    description TEXT,
     create_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     update_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE UNIQUE INDEX bookmarks_collection_id_url_user_id_idx ON bookmarks(collection_id, url, user_id);
+
 CREATE TABLE tags (
-    id BIGSERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
+
     CONSTRAINT tags_name_check CHECK (name ~ '^[a-zA-Z0-9_-]+$') 
 );
 
 CREATE TABLE bookmark_tags (
-    bookmark_id BIGINT REFERENCES bookmarks(id) ON DELETE CASCADE,
-    tag_id BIGINT REFERENCES tags(id) ON DELETE CASCADE,
+    bookmark_id UUID REFERENCES bookmarks(id) ON DELETE CASCADE,
+    tag_id UUID REFERENCES tags(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    
     PRIMARY KEY (bookmark_id, tag_id)
 );
